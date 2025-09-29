bb3acd7e05927e84f481f8aabe5b357bdaa34b11
tankvaishali <vaishalitank28603@gmail.com>
2025-09-29 18:47:31 +0530

    new line start note

--- src/Component/Buynow.jsx
+++ src/Component/Buynow.jsx
@@ -165,11 +165,12 @@ function Buynow() {
                                         </div>
 
 
-                                        <div className="text-dark fw-bold my-2" style={{ fontSize: "14.5px" }}>
+                                        <div className="text-dark fw-bold mt-3" style={{ fontSize: "14.5px" }}>
                                             Note : Registration is compulsory for childrens above 10 years.
+                                        </div>
+                                        <div className="text-dark fw-bold mt-1 mb-3" style={{ fontSize: "14.5px" }}>
                                            We will reach out to you promptly once the pass is available.
                                         </div>
-                                     
 
                                         <div className="w-100">
                                             <Link to={""} className="text-decoration-none">
