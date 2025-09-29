615577eaefdb3d640c3b0ad7c776cf1d2a1e32dc
Mital-123 <mailto:mittalvaghela2003@gmail.com>
2025-09-29 18:31:36 +0530

    note change

--- src/Component/Buynow.jsx
+++ src/Component/Buynow.jsx
@@ -168,7 +168,7 @@ function Buynow() {
                                             Note : Registration is compulsory for childrens above 10 years.
                                         </div>
                                         <div className="text-dark fw-bold mt-1 mb-3" style={{ fontSize: "14.5px" }}>
-                                            Note : You will be contacted when the pass becomes available shortly.
+                                            Note : We will reach out to you promptly once the pass is available.
                                         </div>
 
                                         <div className="w-100">
