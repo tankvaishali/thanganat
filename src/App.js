bd566ffc2bd5248596ff869c66eb51d4b934019b
tankvaishali <vaishalitank28603@gmail.com>
2025-09-29 16:41:45 +0530

    scrolltotop file created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Component/Homepage';
 import Buynow from './Component/Buynow';
 import Datalist from './Component/Datalist';
+import ScrollToTop from './Component/ScrollToTop';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/buynow' element={<Buynow />} />
